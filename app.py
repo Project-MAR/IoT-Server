51d3a495a0379afa973db25964d11be45db9663d
Atitep <atitep.ram@gmail.com>
2018-05-20 16:12:40 +0700

    add push to line

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from flask import Flask, request, abort, make_response
 from flask_httpauth import HTTPBasicAuth
-import os
+import os, requests, json 
 
-import requests, json
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError, LineBotApiError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
 
 app = Flask(__name__)
 
 CHANNEL_SECRET = os.environ.get('provider_channel_secret')
+CHANNEL_ACCESS_TOKEN = os.environ.get('provider_channel_access_token')
+
+USER_TOP_TOKEN = os.environ.get('user_top_id')
+USER_RAM_TOKEN = os.environ.get('user_ram_id')
+
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
 
 @app.route('/')
 def index():
@@ -33,6 +49,18 @@ def callback():
     result = result + 'user message: ' + user_message 
 
     return result
+
+@app.route('/push_to_line', methods=['POST'])
+def push_to_line():
+    payload     = request.get_json()
+    json_string = json.dumps(payload)
+    data_dict   = json.loads(json_string)
+
+    user_name = data_dict['name']
+    user_id   = data_dict['id']
+    user_message = data_dict['message']
+
+    line_bot_api.push_message(USER_TOP_TOKEN, TextSendMessage(text=user_message))
 
 if __name__ == "__main__":
 	app.run()
